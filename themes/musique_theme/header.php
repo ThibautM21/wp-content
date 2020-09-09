4ef1d6eb4b40773800df557a3ee26914becb35d4
ThibautM <thibaut.moy@hotmail.fr>
2020-09-09 11:34:19 +0200

    Ajout des classes dans body et maj du style pour le décalage de 32px

--- themes/musique_theme/header.php
+++ themes/musique_theme/header.php
@@ -7,8 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>
-        <?php bloginfo( 'name'); ?>
+        <?php bloginfo('name'); ?>
     </title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+
+    <?php wp_body_open(); ?>
