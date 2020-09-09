083436393986915d742f8985bd8645835b9768b9
fflorent21 <florent.projets@gmail.com>
2020-09-09 17:19:43 +0200

    modifflo

--- themes/musique_theme/footer.php
+++ themes/musique_theme/footer.php
@@ -1,65 +1,53 @@
 <!--Footer-->
-<footer class="page-footer text-center font-small mt-4 wow fadeIn">
-
-    <!--Call to action-->
-    <div class="pt-4">
-        <a class="btn btn-outline-white" href="https://mdbootstrap.com/getting-started/" target="_blank" role="button">Download MDB
-            <i class="fa fa-download ml-2"></i>
-        </a>
-        <a class="btn btn-outline-white" href="https://mdbootstrap.com/bootstrap-tutorial/" target="_blank" role="button">Start free tutorial
-            <i class="fa fa-graduation-cap ml-2"></i>
-        </a>
+<footer class="page-footer text-center font-small mt-4 wow bg-dark fadeIn">
+    <div class=row>
+        <div class="col-lg-4 pt-5">
+            <h2 class="mb-4">Coordonnées</h2>
+            <address>
+            <p><i class="fa fa-map-marker"></i> 1 Bis rue Lutèce, 75004 PARIS</p>
+            <a href="tel:+33143547519"><i class="fa fa-phone"></i> 01 43 54 75 19</a>
+            </address> 
+            <div>
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.198279250918!2d2.3441018156971314!3d48.85442930893958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e04e0c9aa1%3A0xadf406cffe4d0399!2s1%20Rue%20de%20Lut%C3%A8ce%2C%2075004%20Paris!5e0!3m2!1sfr!2sfr!4v1599659808989!5m2!1sfr!2sfr" width="150" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>              
+            </div>
+        </div>
+         <div class="col-lg-4 pt-2 d-flex flex-column align-self-center">
+            <h2 class="mb-4">Newsletter</h2>
+            <form method="POST" action="#" class="d-flex justify-content-center" class="subscribe">
+                <input type="text" class="form-control mr-3" placeholder="Email" style="width:200px;">
+                <input type="submit" value="Envoyer" class="btn btn-outline-light btn-sm mb-2">
+            </form>
+        </div>
+        <div class="col-lg-4 pt-2 d-flex flex-column align-self-center">
+            <h2 class="mb-4">Liens</h2>
+            <ul class="list-unstyled ">
+                <li><a href="#">À propos de nous</a></li>
+                <li><a href="#">Conditions d'utilisation</a></li>
+                <li><a href="#">Confidentialité</a></li>
+                <li><a href="#">Nous contacter</a></li>
+            </ul> 
+        </div>             
     </div>
-    <!--/.Call to action-->
-
-    <hr class="my-4">
-
     <!-- Social icons -->
     <div class="pb-4">
-        <a href="https://www.facebook.com/mdbootstrap" target="_blank">
-            <i class="fa fa-facebook mr-3"></i>
+        <a href="" target="_blank">
+            <i class="fa fa-facebook fa-2x mr-3"></i>
         </a>
-
-        <a href="https://twitter.com/MDBootstrap" target="_blank">
-            <i class="fa fa-twitter mr-3"></i>
+        <a href="" target="_blank">
+            <i class="fa fa-twitter fa-2x mr-3"></i>
         </a>
-
-        <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
-            <i class="fa fa-youtube mr-3"></i>
+        <a href="" target="_blank">
+            <i class="fa fa-youtube fa-2x mr-3"></i>
         </a>
-
-        <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
-            <i class="fa fa-google-plus mr-3"></i>
+        <a href="" target="_blank">
+            <i class="fa fa-instagram fa-2x mr-3"></i>
         </a>
-
-        <a href="https://dribbble.com/mdbootstrap" target="_blank">
-            <i class="fa fa-dribbble mr-3"></i>
-        </a>
-
-        <a href="https://pinterest.com/mdbootstrap" target="_blank">
-            <i class="fa fa-pinterest mr-3"></i>
-        </a>
-
-        <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
-            <i class="fa fa-github mr-3"></i>
-        </a>
-
-        <a href="http://codepen.io/mdbootstrap/" target="_blank">
-            <i class="fa fa-codepen mr-3"></i>
+        <a href="" target="_blank">
+            <i class="fa fa-twitch fa-2x mr-3"></i>
         </a>
     </div>
-    <!-- Social icons -->
 
     <!--Copyright-->
-    <div class="footer-copyright py-3">
-        © 2018 Copyright:
-        <a href="https://mdbootstrap.com/bootstrap-tutorial/" target="_blank"> MDBootstrap.com </a>
+    <div class="py-3">
+       AlloMusic © 2020 Copyright    
     </div>
-    <!--/.Copyright-->
-<?php wp_footer(); ?>
-</body>
-<script>
-$("#mdb-navigation > ul > li").addClass("page-item")
-$("#mdb-navigation > ul > li > a").addClass("page-link")
-</script>
-</html>
--- themes/musique_theme/header.php
+++ themes/musique_theme/header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <!-- Required meta tags always come first -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/navbar.css" type="text/css">
     <title>
         <?php bloginfo('name'); ?>
     </title>
@@ -14,3 +15,34 @@
 <body <?php body_class(); ?>>
 
     <?php wp_body_open(); ?>
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark sticky-top">
+    <div class="container-fluid">
+    <div class="navbar-brand"><i class="fa fa-headphones fa-2x"></i> AlloMusic</div>
+    
+    <!-- Boutton en responsive  -->
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarText">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <!-- LES LIENS -->
+    <div class="collapse navbar-collapse justify-content-center" id="navbarText">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="#" class="nav-link">Actualités</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Tendances</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Avis</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Vidéos</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Evénements</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
