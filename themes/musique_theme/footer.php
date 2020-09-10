5b3cc5684b36d1300d0826378fbc34a5653c85cd
ThibautM <thibaut.moy@hotmail.fr>
2020-09-10 08:52:53 +0200

    Fix dans le style de 'videos'

--- themes/musique_theme/footer.php
+++ themes/musique_theme/footer.php
@@ -6,9 +6,9 @@
             <address>
             <p><i class="fa fa-map-marker"></i> 1 Bis rue Lutèce, 75004 PARIS</p>
             <a href="tel:+33143547519"><i class="fa fa-phone"></i> 01 43 54 75 19</a>
-            </address> 
+            </address>
             <div>
-                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.198279250918!2d2.3441018156971314!3d48.85442930893958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e04e0c9aa1%3A0xadf406cffe4d0399!2s1%20Rue%20de%20Lut%C3%A8ce%2C%2075004%20Paris!5e0!3m2!1sfr!2sfr!4v1599659808989!5m2!1sfr!2sfr" width="150" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>              
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.198279250918!2d2.3441018156971314!3d48.85442930893958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e04e0c9aa1%3A0xadf406cffe4d0399!2s1%20Rue%20de%20Lut%C3%A8ce%2C%2075004%20Paris!5e0!3m2!1sfr!2sfr!4v1599659808989!5m2!1sfr!2sfr" width="150" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
          <div class="col-lg-4 pt-2 d-flex flex-column align-self-center">
@@ -25,8 +25,8 @@
                 <li><a href="#">Conditions d'utilisation</a></li>
                 <li><a href="#">Confidentialité</a></li>
                 <li><a href="#">Nous contacter</a></li>
-            </ul> 
-        </div>             
+            </ul>
+        </div>
     </div>
     <!-- Social icons -->
     <div class="pb-4">
@@ -49,7 +49,7 @@
 
     <!--Copyright-->
     <div class="py-3">
-       AlloMusic © 2020 Copyright    
+       AlloMusic © 2020 Copyright
     </div>
 </footer>
 
